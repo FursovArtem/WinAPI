d48bb5d689ea51571fa41831d44ffa87eed55f34
Artem <kanshounomatenrou@gmail.com>
2024-01-18 12:15:06 +0400

    text editor

---
+++ TextEditor/main.cpp
@@ -0,0 +1,290 @@
+﻿#include <Windows.h>
+#include <Richedit.h>
+#include "resource.h"
+
+CONST CHAR g_sz_CLASS_NAME[] = "My text editor";
+
+INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+INT CALLBACK DlgProcAbout(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+BOOL LoadTextFileToEdit(HWND hEdit, LPCSTR lpszFileName);
+BOOL SaveTextFileFromEdit(HWND hEdit, LPCSTR lpszFileName);
+VOID SelectFont(HWND hwnd);
+HFONT g_hFont = (HFONT)GetStockObject(DEFAULT_GUI_FONT);
+COLORREF g_RGB_Text = RGB(0, 0, 0);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	WNDCLASSEX wc;
+	ZeroMemory(&wc, sizeof(wc));
+
+	wc.cbSize = sizeof(wc);
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.style = 0;
+
+	wc.hIcon = (HICON)LoadImage(hInstance, "res\\main\\text_editor.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
+	wc.hIconSm = (HICON)LoadImage(hInstance, "res\\main\\text_editor_sm.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
+	wc.hCursor = LoadCursor(hInstance, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+
+	wc.hInstance = hInstance;
+	wc.lpszClassName = g_sz_CLASS_NAME;
+	wc.lpfnWndProc = (WNDPROC)WndProc;
+	wc.lpszMenuName = MAKEINTRESOURCE(IDR_MAIN_MENU);
+
+	if (!RegisterClassEx(&wc))
+	{
+		MessageBox(NULL, "Class registration failed", "Error", MB_OK | MB_ICONERROR);
+		return 0;
+	}
+
+	HWND hwnd = CreateWindowEx
+	(
+		NULL, g_sz_CLASS_NAME, g_sz_CLASS_NAME, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+		NULL, NULL, hInstance, NULL
+	);
+
+	if (hwnd == 0)
+	{
+		MessageBox(NULL, "Window creation failed", "Error", MB_OK | MB_ICONERROR);
+		return 0;
+	}
+
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return msg.wParam;
+}
+
+INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	static CHAR szFileName[MAX_PATH]{};
+	static CHAR* szFileText[MAX_PATH]{};
+	static BOOL onSave = TRUE;
+	switch (uMsg)
+	{
+	case WM_CREATE:
+	{
+		LoadLibrary("riched20.dll");
+		RECT rect;
+		GetWindowRect(hwnd, &rect);
+		HWND hEdit = CreateWindowEx
+		(
+			NULL, RICHEDIT_CLASS, "", WS_CHILD | WS_VISIBLE | ES_MULTILINE | WS_HSCROLL | WS_VSCROLL,
+			0, 0, rect.right - rect.left, rect.bottom - rect.top,
+			hwnd, (HMENU)IDC_EDIT, GetModuleHandle(NULL), NULL
+		);
+		g_hFont = CreateFont(32, 11, 0, 0, FW_MEDIUM, FALSE, FALSE, FALSE,
+			RUSSIAN_CHARSET, OUT_TT_PRECIS, CLIP_CHARACTER_PRECIS, ANTIALIASED_QUALITY, FF_DONTCARE, "Tahoma");
+		SendMessage(hEdit, WM_SETFONT, (WPARAM)g_hFont, TRUE);
+		SetFocus(hEdit);
+
+		RegisterHotKey(hwnd, ID_FILE_OPEN + 10000, MOD_CONTROL, 'O');
+		RegisterHotKey(hwnd, ID_FILE_SAVE + 10000, MOD_CONTROL, 'S');
+	}
+	break;
+	case WM_SIZE:
+	{
+		RECT rect;
+		GetClientRect(hwnd, &rect);
+		SetWindowPos(GetDlgItem(hwnd, IDC_EDIT), NULL, rect.left, rect.top, rect.right, rect.bottom, SWP_NOZORDER);
+	}
+	break;
+	case WM_CTLCOLOREDIT:
+	{
+		HDC hdc = (HDC)wParam;
+		SetBkMode(hdc, OPAQUE);
+		SetBkColor(hdc, g_RGB_Text);
+		HBRUSH hBrush = CreateSolidBrush(g_RGB_Text);
+		SetTextColor(hdc, g_RGB_Text);
+		return (LRESULT)hBrush;
+	}
+		break;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case ID_FILE_OPEN:
+		{
+			OPENFILENAME ofn;
+			ZeroMemory(&ofn, sizeof(ofn));
+
+			ofn.lStructSize = sizeof(ofn);
+			ofn.hwndOwner = hwnd;
+			ofn.lpstrFilter = "Text files: (*.txt)\0*.txt\0All files: (*.*)\0*.*\0\0";
+			ofn.lpstrFile = szFileName;
+			ofn.nMaxFile = MAX_PATH;
+			ofn.Flags = OFN_EXPLORER | OFN_FILEMUSTEXIST | OFN_HIDEREADONLY;
+			ofn.lpstrDefExt = "txt";
+
+			if (GetOpenFileName(&ofn)) onSave = LoadTextFileToEdit(GetDlgItem(hwnd, IDC_EDIT), szFileName);
+		}
+		break;
+		case ID_FILE_SAVE:
+			if (strlen(szFileName))
+			{
+				onSave = SaveTextFileFromEdit(GetDlgItem(hwnd, IDC_EDIT), szFileName);
+				SendMessage(GetDlgItem(hwnd, IDC_EDIT), EM_SETMODIFY, FALSE, 0);
+			}
+			else SendMessage(hwnd, WM_COMMAND, ID_FILE_SAVEAS, 0);
+			break;
+		case ID_FILE_SAVEAS:
+		{
+			OPENFILENAME ofn;
+			ZeroMemory(&ofn, sizeof(ofn));
+
+			ofn.lStructSize = sizeof(ofn);
+			ofn.hwndOwner = hwnd;
+			ofn.lpstrFilter = "Text files: (*.txt)\0*.txt\0All files: (*.*)\0*.*\0\0";
+			ofn.lpstrFile = szFileName;
+			ofn.nMaxFile = MAX_PATH;
+			ofn.Flags = OFN_EXPLORER | OFN_PATHMUSTEXIST | OFN_HIDEREADONLY | OFN_OVERWRITEPROMPT;
+			ofn.lpstrDefExt = "txt";
+
+			if (GetSaveFileName(&ofn))
+			{
+				onSave = SaveTextFileFromEdit(GetDlgItem(hwnd, IDC_EDIT), szFileName);
+				SendMessage(GetDlgItem(hwnd, IDC_EDIT), EM_SETMODIFY, FALSE, 0);
+			}
+		}
+		break;
+		case ID_FORMAT_FONT: SelectFont(hwnd); break;
+		case ID_HELP_ABOUT:
+			DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG_ABOUT), hwnd, (DLGPROC)DlgProcAbout, 0);
+			break;
+		}
+		break;
+	case WM_HOTKEY:
+	{
+		switch (wParam)
+		{
+		case ID_FILE_OPEN + 10000:
+		case ID_FILE_SAVE + 10000: SendMessage(hwnd, WM_COMMAND, LOWORD(wParam - 10000), 0); break;
+		}
+	}
+	break;
+	case WM_DESTROY: PostQuitMessage(0); break;
+	case WM_CLOSE:
+		if (SendMessage(GetDlgItem(hwnd, IDC_EDIT), EM_GETMODIFY, 0, 0))
+		{
+			onSave = FALSE;
+			switch (MessageBox(hwnd, "Сохранить изменения в файле?", "Хотите ли вы", MB_YESNOCANCEL | MB_ICONQUESTION))
+			{
+			case IDYES: SendMessage(hwnd, WM_COMMAND, ID_FILE_SAVE, 0);
+				if (onSave)DestroyWindow(hwnd); break;
+			case IDNO: DestroyWindow(hwnd);
+			case IDCANCEL: break;
+			}
+		}
+		else DestroyWindow(hwnd);
+		break;
+	default: return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
+	return FALSE;
+}
+
+INT CALLBACK DlgProcAbout(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+		break;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case IDOK: EndDialog(hwnd, 0);
+		}
+		break;
+	case WM_CLOSE: EndDialog(hwnd, 0);
+	}
+	return FALSE;
+}
+
+BOOL LoadTextFileToEdit(HWND hEdit, LPCSTR lpszFileName)
+{
+	BOOL bSuccess = FALSE;
+	HANDLE hFile = CreateFile(lpszFileName, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, 0, NULL);
+	if (hFile != INVALID_HANDLE_VALUE)
+	{
+		DWORD dwFileSize = GetFileSize(hFile, NULL);
+		if (dwFileSize != UINT_MAX)
+		{
+			LPSTR lpszFileText = (LPSTR)GlobalAlloc(GPTR, dwFileSize + 1);
+			if (lpszFileText)
+			{
+				DWORD dwRead;
+				if (ReadFile(hFile, lpszFileText, dwFileSize, &dwRead, NULL))
+				{
+					lpszFileText[dwFileSize] = 0;
+					if (SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)lpszFileText)) bSuccess = TRUE;
+				}
+				GlobalFree(lpszFileText);
+			}
+			CloseHandle(hFile);
+		}
+	}
+	return bSuccess;
+}
+
+BOOL SaveTextFileFromEdit(HWND hEdit, LPCSTR lpszFileName)
+{
+	BOOL bSuccsess = FALSE;
+	HANDLE hFile = CreateFile(lpszFileName, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (hFile != INVALID_HANDLE_VALUE)
+	{
+		DWORD dwTextLength = SendMessage(hEdit, WM_GETTEXTLENGTH, 0, 0) + 1;
+		if (dwTextLength > 0)
+		{
+			LPSTR lpszText = (LPSTR)GlobalAlloc(GPTR, dwTextLength);
+			if (lpszText)
+			{
+				if (SendMessage(hEdit, WM_GETTEXT, dwTextLength, (LPARAM)lpszText))
+				{
+					DWORD dwWritten;
+					if (WriteFile(hFile, lpszText, dwTextLength, &dwWritten, 0))bSuccsess = TRUE;
+				}
+				GlobalFree(lpszText);
+			}
+		}
+		CloseHandle(hFile);
+	}
+	return bSuccsess;
+}
+VOID SelectFont(HWND hwnd)
+{
+	HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
+
+	CHOOSEFONT cf;
+	LOGFONT lf;
+	ZeroMemory(&cf, sizeof(cf));
+	ZeroMemory(&lf, sizeof(lf));
+
+	GetObject(g_hFont, sizeof(LOGFONT), &lf);
+
+	cf.lStructSize = sizeof(cf);
+	cf.hwndOwner = hwnd;
+	cf.Flags = CF_EFFECTS | CF_INITTOLOGFONTSTRUCT | CF_SCREENFONTS;
+	cf.hInstance = GetModuleHandle(NULL);
+	cf.lpLogFont = &lf;
+	cf.rgbColors = g_RGB_Text;
+
+	if (ChooseFont(&cf))
+	{
+		HFONT hf = CreateFontIndirect(&lf);
+		if (hf)g_hFont = hf;
+		else MessageBox(hwnd, "Font creation failed", "Error", MB_OK | MB_ICONERROR);
+	}
+	SendMessage(hEdit, WM_SETFONT, (WPARAM)g_hFont, TRUE);
+	g_RGB_Text = cf.rgbColors;
+
+	HDC hdc = GetDC(hEdit);
+	SendMessage(hwnd, WM_CTLCOLOREDIT, (WPARAM)hdc, (LPARAM)hEdit);
+	DeleteDC(hdc);
+}
